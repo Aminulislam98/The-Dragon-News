ce0771b6297a67777d54c5769854d244d93cc92d
Aminul islam <aminurrohman98@gmail.com>
2026-04-24 21:15:08 +0100

    header added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Header></Header>
         <Navbar></Navbar>
         <main>{children}</main>
         <Footer></Footer>
